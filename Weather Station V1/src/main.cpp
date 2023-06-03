cee56f89b6f37d6adb839fbe443e2dc5af83b2ac
adishow <adilsoncabraldev@gmail.com>
2023-06-03 17:16:08 +0100

    Configure an ESP8266 as a Wi-Fi Access Point

--- Weather Station V1/src/main.cpp
+++ Weather Station V1/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
 // put function declarations here:
-int myFunction(int, int);
+int configureWifiAccessPoint(String);
 
+// put constants here:
+const char *ssid = "AC-ESP8266";
+const char *password = "987654321";
+
+IPAddress local_IP(192,168,4,22);
+IPAddress gateway(192,168,4,9);
+IPAddress subnet(255,255,255,0);
+
+// put function definitions here:
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println();
+
+  Serial.print("Setting soft-AP configuration ... ");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+
+  Serial.print("Setting soft-AP ... ");
+  Serial.println(WiFi.softAP(ssid,password) ? "Ready" : "Failed!");
+  //WiFi.softAP(ssid);
+  //WiFi.softAP(ssid, password, channel, hidden, max_connection)
+  
+  Serial.print("Soft-AP IP address = ");
+  Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  Serial.print("[Server Connected] ");
+  Serial.println(WiFi.softAPIP());
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(500);
 }
