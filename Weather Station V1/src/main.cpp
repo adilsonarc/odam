a1c15de7334d62d37d0bb1b6b53d85ac9fb5d83d
adishow <adilsoncabraldev@gmail.com>
2023-06-03 18:37:01 +0100

    Refactor

--- Weather Station V1/src/main.cpp
+++ Weather Station V1/src/main.cpp
@@ -2,38 +2,62 @@
 #include <ESP8266WiFi.h>
 
 // put function declarations here:
-int configureWifiAccessPoint(String);
+void configureMonitor(int);
+void configureWifiAccessPoint(IPAddress, IPAddress, IPAddress, char *);
+void configureWifiAccessPoint(IPAddress, IPAddress, IPAddress, char *, char *);
+void configureWifiAccessPoint(IPAddress, IPAddress, IPAddress, char *, char *, int, int, int);
 
-// put constants here:
-const char *ssid = "AC-ESP8266";
-const char *password = "987654321";
+void monitorAccessPoint();
 
-IPAddress local_IP(192,168,4,22);
-IPAddress gateway(192,168,4,9);
-IPAddress subnet(255,255,255,0);
+// put constants and variables here:
+int loop_delay_time = 500; // miliseconds
+int monitor_speed = 115200;
+
+IPAddress local_IP(192, 168, 4, 22);
+IPAddress gateway(192, 168, 4, 9);
+IPAddress subnet(255, 255, 255, 0);
+
+char *ssid = "AC-ESP8266";
+char *password = "987654321";
 
 // put function definitions here:
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println();
+  configureMonitor(monitor_speed);
+  configureWifiAccessPoint(local_IP, gateway, subnet, ssid, password);
+}
 
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  monitorAccessPoint();
+
+  delay(loop_delay_time);
+}
+
+// put configuration functions here:
+void configureMonitor(int monitor_speed)
+{
+  Serial.begin(monitor_speed);
+  Serial.println();
+}
+
+void configureWifiAccessPoint(IPAddress local_IP, IPAddress gateway, IPAddress subnet, char *ssid, char *password)
+{
   Serial.print("Setting soft-AP configuration ... ");
   Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
 
   Serial.print("Setting soft-AP ... ");
-  Serial.println(WiFi.softAP(ssid,password) ? "Ready" : "Failed!");
-  //WiFi.softAP(ssid);
-  //WiFi.softAP(ssid, password, channel, hidden, max_connection)
-  
+  boolean isReady = WiFi.softAP(ssid, password);
+
+  Serial.println(isReady ? "Ready" : "Failed!");
+
   Serial.print("Soft-AP IP address = ");
   Serial.println(WiFi.softAPIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("[Server Connected] ");
-  Serial.println(WiFi.softAPIP());
-
-  delay(500);
+// put monitor functions here:
+void monitorAccessPoint() {
+  Serial.println("Soft-AP IP address = " + WiFi.softAPIP().toString());
 }
