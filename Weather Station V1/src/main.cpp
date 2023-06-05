cd073f2619ab37c2fe9d9d70fbd1d577b35437aa
adishow <adilsoncabraldev@gmail.com>
2023-06-05 15:05:01 +0100

    Establishing a Wi-Fi connection

--- Weather Station V1/src/main.cpp
+++ Weather Station V1/src/main.cpp
@@ -1,13 +1,45 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h> // Include the Wi-Fi library
 
 // put function declarations here:
+void establishingWiFiConnection();
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
+  establishingWiFiConnection();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
+void establishingWiFiConnection()
+{
+  const char *ssid = "SSID";         // The SSID (name) of the Wi-Fi network you want to connect to
+  const char *password = "PASSWORD"; // The password of the Wi-Fi network
+
+  Serial.begin(115200); // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+
+  WiFi.begin(ssid, password); // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i);
+    Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
+}
